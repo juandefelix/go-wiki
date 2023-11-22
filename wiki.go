2171dd783cc594db8dfc6a0d98c2e7f9cc399fc6
Juan Ortiz <juanfortizd@gmail.com>
2023-11-22 11:59:42 -0600

    initial commit

---
+++ wiki.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r http.Request) {
+	start := len("/view/")
+	title := r.URL.Path[start:]
+	p, _ = loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.title, p.body)
+}
+
+func main() {
+	// pageTitle := "Western movie"
+	// p1 := &Page{Title: pageTitle, Body: []byte("Once upon a time in the west")}
+	// p1.save()
+	// p2, _ := loadPage(pageTitle)
+	// fmt.Println(string(p2.Body))
+	http.HandleFunction("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8000"), nil)
+}
